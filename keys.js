00fbcf4142d85c2b5cb63d7787e5826de8a1c2df
Curtis Lytle <curtlytle@gmail.com>
2017-11-18 17:22:48 -0700

    Now using inquirer for better user command line input.  Finished moviedb feature and Twitter feature.

--- keys.js
+++ keys.js
@@ -1,10 +1,10 @@
 //console.log('this is loaded');
 
 var twitterKeys = {
-    consumer_key: '<input here>',
-    consumer_secret: '<input here>',
-    access_token_key: '<input here>',
-    access_token_secret: '<input here>'
+    consumer_key: 'oQoNocZCq0TPXS0zWqnwJngm5',
+    consumer_secret: 'wyPP4eyaof8whyjjOyi7jCvGNAA0GAv7qhRX02xoEKJKAi5bpU',
+    access_token_key: '932032110478508034-DbfClngVoCBG4BmQFKvYVdG4SghFD70',
+    access_token_secret: '9Kisw2fhLzLoUcX3NroFWEWhzXmJdc6unbQsBHEGYNA8Q'
 };
 
 module.exports = twitterKeys;
--- liri.js
+++ liri.js
@@ -1,30 +1,70 @@
 // main js file
-var args = process.argv;
-var arg1 = args[2];
+var choice = "";
+var userInput = "";
 // Core node package for reading and writing files
 var fs = require("fs");
 var request = require("request");
 var twitterKeys = require("./keys.js");
+var inq = require("inquirer");
+var Twitter = require('twitter');
 
-switch (arg1) {
-    case "my-tweets":
+inq.prompt([
+    {
+        type: "list",
+        message: "What do you want to do?",
+        name: "userChoice",
+        choices: ["movie-this", "my-tweets", "spotify-this-song", "do-what-it-says"]
+    }
+]).then(function (userResponse) {
+    // Use user feedback for... whatever!!
+    choice = userResponse.userChoice;
+    if (choice === "movie-this" || choice === "spotify-this-song") {
+        var msg = "";
+        if (choice === "movie-this") {
+            msg = "Name the movie.";
+        } else {
+            msg = "Name the song.";
+        }
+        inq.prompt([
+            {
+                type: "input",
+                message: msg,
+                name: "userInput"
+            }
+        ]).then(function (uResponse) {
+            userInput = uResponse.userInput;
+            if (choice === "movie-this") {
+                movieThis();
+            } else {
+                spotifyThisSong();
+            }
+        });
+    } else if (choice === "my-tweets") {
         myTweets();
-        break;
-
-    case "spotify-this-song":
-        spotifyThisSong();
-        break;
-
-    case "movie-this":
-        movieThis();
-        break;
-
-    case "do-what-it-says":
+    } else if (choice === "do-what-it-says") {
         doWhatItSays();
-        break;
-}
+    }
+});
 
 function myTweets() {
+    var client = new Twitter({
+        consumer_key: twitterKeys.consumer_key,
+        consumer_secret: twitterKeys.consumer_secret,
+        access_token_key: twitterKeys.access_token_key,
+        access_token_secret: twitterKeys.access_token_secret
+    });
+
+    var params = {};
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+        if (!error) {
+            for (var i = 0; i < tweets.length && i < 19; i++) {
+                var tweet = tweets[i];
+                console.log("* Tweet created at: " + tweet.created_at);
+                console.log("* Tweet: " + tweet.text);
+            }
+            // console.log(tweets);
+        }
+    });
 
 }
 
@@ -33,13 +73,12 @@ function spotifyThisSong() {
 }
 
 function movieThis() {
-    var movieName = "";
-    if (args.length !== 4) {
+    if (userInput.length === 0) {
         movieName = "Mr. Nobody";
     } else {
-        movieName = args[3];
-
+        movieName = userInput;
     }
+
     var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
     // Then run a request to the OMDB API with the movie specified
     request(queryUrl, function (error, response, body) {
@@ -49,8 +88,21 @@ function movieThis() {
             var movie = JSON.parse(body);
             // console.log(movie);
             console.log("* " + movie.Title);
-            console.log("* " + movie.Year);
-            console.log("* " + movie.imdbRating);
+            console.log("* Year it came out: " + movie.Year);
+            console.log("* ImbDb Rating: " + movie.imdbRating);
+            var ratings = movie.Ratings;
+            for (var i = 0; i < ratings.length; i++) {
+                var rat = ratings[i];
+                // console.log("Rat: " + rat);
+                if (rat.Source === "Rotten Tomatoes") {
+                    console.log("* Rotten Tomatoes Rating: " + rat.Value);
+                    break;
+                }
+            }
+            console.log("* Country where produced: " + movie.Country);
+            console.log("* Language: " + movie.Language);
+            console.log("* Plot: " + movie.Plot);
+            console.log("* Actors: " + movie.Actors);
         }
     });
 
